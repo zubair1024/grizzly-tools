f1891fa2edc232f17618f9cf67515788d80cd1c6
Zubair Ahmed <zubair1024@gmail.com>
2023-02-24 15:41:11 +0100

    adding nato to text convertor

--- src/pages/text-to-nato.tsx
+++ src/pages/text-to-nato.tsx
@@ -1,16 +1,25 @@
 import Layout from '@/components/Layout';
 import routes from '@/data/routes';
+import { word2words } from 'node.nato-phonetics.lib';
 import { useState } from 'react';
 
 const route = routes.filter((i) => i.route === 'text-to-nato')?.[0];
 
 const TextToNatoScreen = () => {
   const [inputText, setInputText] = useState('');
+  const [outputText, setOutputText] = useState('');
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    const output = word2words(value);
+    setInputText(value);
+    setOutputText(output.join(' '));
+  };
 
   return (
     <>
       <Layout>
-        <div className="max-w-xl mx-auto px-5 py-5">
+        <div className="max-w-xl min-h-screen mx-auto px-5 py-5">
           <div>
             <h1>{route.title}</h1>
             <p>{route.desc}</p>
@@ -21,16 +30,16 @@ const TextToNatoScreen = () => {
                 type="text"
                 placeholder="Type here"
                 value={inputText}
-                onChange={(e) => setInputText(e.target.value)}
+                onChange={handleInputChange}
                 className="input input-bordered input-lg w-full "
               />
             </div>
             <div>
-              <input
-                type="text"
-                placeholder="testing"
-                className="input input-bordered input-lg w-full"
-              />
+              <textarea
+                className="textarea textarea-bordered textarea-lg w-full"
+                rows={5}
+                value={outputText}
+              ></textarea>
             </div>
           </div>
         </div>
