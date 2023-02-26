37c038d2354c376c4828652972e1b7c8ebe6f4a9
Zubair Ahmed <zubair1024@gmail.com>
2023-02-26 13:48:29 +0100

    adding new ip-address look up page

--- src/pages/my-ip-address.tsx
+++ src/pages/my-ip-address.tsx
@@ -8,35 +8,54 @@ import { useCallback, useEffect, useState } from 'react';
 const route = routes.filter((i) => i.route === 'my-ip-address')?.[0];
 
 interface IIpLookupData {
-  query: string;
+  ip: string;
   city: string;
-  country: string;
+  country_name: string;
   isp: string;
-  lat: string;
-  lon: string;
-  timezone: string;
-  zip: string;
+  latitude: string;
+  longitude: string;
+  timezone: { name: string };
+  zipcode: string;
 }
 
-async function fetchIpDetails(): Promise<IIpLookupData> {
-  const res = await axios.get('http://ip-api.com/json/');
+async function fetchIpDetails(apiKey: string): Promise<IIpLookupData> {
+  const res = await axios.get(
+    `https://api.ipgeolocation.io/ipgeo?apiKey=${apiKey}`,
+  );
   return res.data;
 }
 
-const RandomPortGeneratorScreen = (props: { geoApiKey: string }) => {
-  const { geoApiKey } = props;
+const RandomPortGeneratorScreen = (props: {
+  GEO_API_KEY: string;
+  IP_GEO_API_KEY: string;
+}) => {
+  const { GEO_API_KEY, IP_GEO_API_KEY } = props;
   const [isLoading, setIsLoading] = useState(false);
   const [lookup, setLookup] = useState<IIpLookupData | null>(null);
 
   const generatePasswords = useCallback(async () => {
     setIsLoading(true);
-    const details = await fetchIpDetails();
-    console.log(details);
-    console.log(
-      `https://maps.geoapify.com/v1/staticmap?style=osm-carto&width=600&height=400&center=lonlat:${lookup?.lon},${lookup?.lat}&zoom=14&apiKey=${geoApiKey}`,
-    );
-    const { query, city, country, isp, lat, lon, timezone, zip } = details;
-    setLookup({ query, city, country, isp, lat, lon, timezone, zip });
+    const details = await fetchIpDetails(IP_GEO_API_KEY);
+    const {
+      ip,
+      city,
+      country_name,
+      isp,
+      latitude,
+      longitude,
+      timezone,
+      zipcode,
+    } = details;
+    setLookup({
+      ip,
+      city,
+      country_name,
+      isp,
+      latitude,
+      longitude,
+      timezone,
+      zipcode,
+    });
     setIsLoading(false);
   }, []);
 
@@ -63,12 +82,10 @@ const RandomPortGeneratorScreen = (props: { geoApiKey: string }) => {
               <>
                 <div className="grid grid-cols-3 text-center items-center justify-center">
                   <p className="text-2xl">Public IP</p>
-                  <p className="text-2xl font-bold">{lookup?.query}</p>
+                  <p className="text-2xl font-bold">{lookup?.ip}</p>
                   <button
                     className="btn btn-ghost"
-                    onClick={() =>
-                      lookup?.query && copyTextToClip(lookup?.query)
-                    }
+                    onClick={() => lookup?.ip && copyTextToClip(lookup?.ip)}
                   >
                     Copy
                   </button>
@@ -85,11 +102,12 @@ const RandomPortGeneratorScreen = (props: { geoApiKey: string }) => {
                 </div>
                 <div className="grid grid-cols-3 text-center items-center justify-center">
                   <p className="text-2xl">Country</p>
-                  <p className="text-2xl font-bold">{lookup?.country}</p>
+                  <p className="text-2xl font-bold">{lookup?.country_name}</p>
                   <button
                     className="btn btn-ghost"
                     onClick={() =>
-                      lookup?.country && copyTextToClip(lookup?.country)
+                      lookup?.country_name &&
+                      copyTextToClip(lookup?.country_name)
                     }
                   >
                     Copy
@@ -105,33 +123,35 @@ const RandomPortGeneratorScreen = (props: { geoApiKey: string }) => {
                     Copy
                   </button>
                 </div>
-                <div className="grid grid-cols-3 text-center items-center justify-center">
+                {/* <div className="grid grid-cols-3 text-center items-center justify-center">
                   <p className="text-2xl">Timezone</p>
-                  <p className="text-2xl font-bold">{lookup?.timezone}</p>
+                  <p className="text-2xl font-bold">{lookup?.timezone.name}</p>
                   <button
                     className="btn btn-ghost"
                     onClick={() =>
-                      lookup?.timezone && copyTextToClip(lookup?.timezone)
+                      lookup?.timezone && copyTextToClip(lookup?.timezone.name)
+                    }
+                  >
+                    Copy
+                  </button>
+                </div> */}
+                <div className="grid grid-cols-3 text-center items-center justify-center">
+                  <p className="text-2xl">ZIP</p>
+                  <p className="text-2xl font-bold">{lookup?.zipcode}</p>
+                  <button
+                    className="btn btn-ghost"
+                    onClick={() =>
+                      lookup?.zipcode && copyTextToClip(lookup?.zipcode)
                     }
                   >
                     Copy
                   </button>
                 </div>
-                <div className="grid grid-cols-3 text-center items-center justify-center">
-                  <p className="text-2xl">ZIP</p>
-                  <p className="text-2xl font-bold">{lookup?.zip}</p>
-                  <button
-                    className="btn btn-ghost"
-                    onClick={() => lookup?.zip && copyTextToClip(lookup?.zip)}
-                  >
-                    Copy
-                  </button>
-                </div>
                 <div>
-                  {lookup?.lat && lookup.lon ? (
+                  {lookup?.latitude && lookup.longitude ? (
                     <div className="flex items-center justify-center">
                       <Image
-                        src={`https://maps.geoapify.com/v1/staticmap?style=osm-carto&width=600&height=400&center=lonlat:${lookup?.lon},${lookup?.lat}&zoom=14&apiKey=${geoApiKey}`}
+                        src={`https://maps.geoapify.com/v1/staticmap?style=osm-carto&width=600&height=400&center=lonlat:${lookup?.longitude},${lookup?.latitude}&zoom=14&apiKey=${GEO_API_KEY}`}
                         height={512}
                         width={512}
                         alt={'address'}
@@ -159,10 +179,12 @@ const RandomPortGeneratorScreen = (props: { geoApiKey: string }) => {
 };
 
 export async function getServerSideProps() {
-  const geoApiKey = process.env.GEO_API_KEY;
+  const GEO_API_KEY = process.env.GEO_API_KEY;
+  const IP_GEO_API_KEY = process.env.IP_GEO_API_KEY;
   return {
     props: {
-      geoApiKey,
+      GEO_API_KEY,
+      IP_GEO_API_KEY,
     },
   };
 }
